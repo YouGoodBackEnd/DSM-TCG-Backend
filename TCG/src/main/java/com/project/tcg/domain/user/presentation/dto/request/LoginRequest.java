16da5f2d83cc77a4483d24f2d1bc8cf96f03de93
rlaisqls <rlaisqls@gmail.com>
2022-07-17 23:29:01 +0900

    ♻️ :: 접근제어자 변경

--- TCG/src/main/java/com/project/tcg/domain/user/presentation/dto/request/LoginRequest.java
+++ TCG/src/main/java/com/project/tcg/domain/user/presentation/dto/request/LoginRequest.java
@@ -1,11 +1,10 @@
 package com.project.tcg.domain.user.presentation.dto.request;
 
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class LoginRequest {
 
     private String accountId;
--- TCG/src/main/java/com/project/tcg/domain/user/presentation/dto/request/SignupRequest.java
+++ TCG/src/main/java/com/project/tcg/domain/user/presentation/dto/request/SignupRequest.java
@@ -1,13 +1,12 @@
 package com.project.tcg.domain.user.presentation.dto.request;
 
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class SignupRequest {
 
     @NotBlank(message = "이름은 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
