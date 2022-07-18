821acb8d03060e82aad9284bc6f409a60121780a
rlaisqls <rlaisqls@gmail.com>
2022-07-18 23:12:45 +0900

    ⚡️ :: expiredAt 추가

--- TCG/src/main/java/com/project/tcg/global/utils/token/dto/TokenResponse.java
+++ TCG/src/main/java/com/project/tcg/global/utils/token/dto/TokenResponse.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.ZonedDateTime;
+
 @Getter
 @Builder
 @AllArgsConstructor
 public class TokenResponse {
 
     private final String accessToken;
+
+    private final ZonedDateTime expiredAt;
 
     private final String refreshToken;
 
