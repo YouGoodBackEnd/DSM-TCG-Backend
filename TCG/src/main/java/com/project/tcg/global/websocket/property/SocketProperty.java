0eaafec74e17489cf0fa0045da750900f87fb835
rlaisqls <rlaisqls@gmail.com>
2022-07-20 23:18:26 +0900

    📑 :: Client, Socket Property

---
+++ TCG/src/main/java/com/project/tcg/global/websocket/property/SocketProperty.java
@@ -0,0 +1,17 @@
+package com.project.tcg.global.websocket.property;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SocketProperty {
+
+    public static final String SUGGEST = "suggest";
+
+    public static final String EMOJI = "emoji";
+
+    public static final String CHAT = "chat";
+
+    public static final String ERROR = "error";
+
+}
