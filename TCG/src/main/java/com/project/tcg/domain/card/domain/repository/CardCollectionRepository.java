6051140387ac67ed56b23840b1bd19f7fbc271c5
rlaisqls <rlaisqls@gmail.com>
2022-07-21 21:57:22 +0900

    🐛 :: Id 복합키

--- TCG/src/main/java/com/project/tcg/domain/card/domain/repository/CardCollectionRepository.java
+++ TCG/src/main/java/com/project/tcg/domain/card/domain/repository/CardCollectionRepository.java
@@ -1,7 +1,12 @@
 package com.project.tcg.domain.card.domain.repository;
 
 import com.project.tcg.domain.card.domain.CardCollection;
+import com.project.tcg.domain.card.domain.CardCollectionId;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CardCollectionRepository extends CrudRepository<CardCollection, Long> {
+import java.util.List;
+
+public interface CardCollectionRepository extends CrudRepository<CardCollection, CardCollectionId> {
+    List<CardCollection> findByUserId(Long userId);
+
 }
