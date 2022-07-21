a69c8910f1aa01cfa55b97d1fde8c3ea0cf39561
rlaisqls <rlaisqls@gmail.com>
2022-07-21 21:57:52 +0900

    🐛 :: NPE

--- TCG/src/main/java/com/project/tcg/domain/card/domain/Card.java
+++ TCG/src/main/java/com/project/tcg/domain/card/domain/Card.java
@@ -1,10 +1,12 @@
 package com.project.tcg.domain.card.domain;
 
+import com.project.tcg.domain.card.domain.enums.CardType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -12,8 +14,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Getter
 @Builder
@@ -27,12 +31,10 @@ public class Card {
     private Long id;
 
     @NotNull
-    @Column(unique = true)
     @Size(max = 30)
     private String name;
 
     @NotNull
-    @Column(nullable = false)
     private String cardImageUrl;
 
     @NotNull
@@ -41,8 +43,15 @@ public class Card {
     private Grade grade;
 
     @NotNull
-    @Column(unique = true)
     @Size(max = 255)
     private String description;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true)
+    private CardType code;
+
+    @OneToMany(mappedBy = "card", cascade = CascadeType.REMOVE)
+    private List<CardCollection> badgeCollections;
 
 }
