9021bc409b5d5f16b188bfd775f7f5a03c6d14c1
rlaisqls <rlaisqls@gmail.com>
2022-07-22 08:58:00 +0900

    📑 :: WebSocketAddMappingSupporter
    📑 ::

---
+++ TCG/src/main/java/com/project/tcg/global/websocket/WebSocketAddMappingSupporter.java
@@ -0,0 +1,62 @@
+package com.project.tcg.global.websocket;
+
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.project.tcg.global.websocket.annotation.SocketController;
+import com.project.tcg.global.websocket.annotation.SocketMapping;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketAddMappingSupporter {
+
+    private final ConfigurableListableBeanFactory beanFactory;
+    private SocketIOServer socketIOServer;
+
+    public void addListeners(SocketIOServer socketIOServer) {
+        this.socketIOServer = socketIOServer;
+        final List<Class<?>> classes = beanFactory.getBeansWithAnnotation(SocketController.class).values()
+                .stream()
+                .map(Object::getClass)
+                .collect(Collectors.toList());
+
+        for (Class<?> cls : classes) {
+            List<Method> methods = findSocketMappingAnnotatedMethods(cls);
+            addSocketServerEventListener(cls, methods);
+        }
+
+    }
+
+    private void addSocketServerEventListener(Class<?> controller, List<Method> methods) {
+        for (Method method : methods) {
+            SocketMapping socketMapping = method.getAnnotation(SocketMapping.class);
+            String endpoint = socketMapping.endpoint();
+            Class<?> dtoClass = socketMapping.requestCls();
+
+            socketIOServer.addEventListener(endpoint, dtoClass, ((client, data, ackSender) -> {
+                List<Object> args = new ArrayList<>();
+                for (Class<?> params : method.getParameterTypes()) {
+                    if (params.equals(SocketIOServer.class)) args.add(socketIOServer);
+                    else if (params.equals(SocketIOClient.class)) args.add(client);
+                    else if (params.equals(dtoClass)) args.add(data);
+                }
+                method.invoke(beanFactory.getBean(controller), args.toArray());
+            }));
+        }
+    }
+
+    private List<Method> findSocketMappingAnnotatedMethods(Class<?> cls) {
+        return Arrays.stream(cls.getMethods())
+                .filter(method -> method.getAnnotation(SocketMapping.class) != null)
+                .collect(Collectors.toList());
+    }
+
+}
