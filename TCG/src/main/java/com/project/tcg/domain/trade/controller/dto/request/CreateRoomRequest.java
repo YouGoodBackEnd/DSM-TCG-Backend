2106fe9cdfcbfcc2813564ff7a2c6b6f1f60cfee
rlaisqls <rlaisqls@gmail.com>
2022-07-22 14:13:30 +0900

    📑 :: CreateRoomRequest

---
+++ TCG/src/main/java/com/project/tcg/domain/trade/controller/dto/request/CreateRoomRequest.java
@@ -0,0 +1,11 @@
+package com.project.tcg.domain.trade.controller.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CreateRoomRequest {
+
+    private String roomName;
+}
