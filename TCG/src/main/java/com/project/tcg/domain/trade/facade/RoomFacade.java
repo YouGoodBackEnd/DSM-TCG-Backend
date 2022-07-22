25b6c507315fe9c9b646b9dc33e1946e82e0f63c
rlaisqls <rlaisqls@gmail.com>
2022-07-22 14:19:25 +0900

    📑 :: RoomFacade

---
+++ TCG/src/main/java/com/project/tcg/domain/trade/facade/RoomFacade.java
@@ -0,0 +1,38 @@
+package com.project.tcg.domain.trade.facade;
+
+import com.project.tcg.domain.trade.domain.Room;
+import com.project.tcg.domain.trade.domain.RoomUser;
+import com.project.tcg.domain.trade.domain.repository.RoomRepository;
+import com.project.tcg.domain.trade.exception.RoomNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class RoomFacade {
+
+    private final RoomRepository roomRepository;
+
+    public Room getRoomById(Long roomId) {
+        return roomRepository.findById(roomId)
+                .orElseThrow(() -> RoomNotFoundException.EXCEPTION);
+    }
+
+    public void roomUsersAcceptFalse(Room room) {
+        room.getRoomUsers().forEach(RoomUser::acceptFalse);
+    }
+
+    public boolean isNotEmptyRoom(Room room) {
+
+        if (room.getRoomUsers().size() == 0) {
+            roomRepository.delete(room);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isNotOverstaffedRoom(Room room) {
+
+        return 2 > room.getRoomUsers().size();
+    }
+}
