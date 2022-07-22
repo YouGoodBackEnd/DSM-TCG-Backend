8d3301babd8c274d5a5e3908d86c7289aab8a414
rlaisqls <rlaisqls@gmail.com>
2022-07-22 14:17:17 +0900

    📑 :: Emojitype

---
+++ TCG/src/main/java/com/project/tcg/domain/trade/domain/Emoji.java
@@ -0,0 +1,31 @@
+package com.project.tcg.domain.trade.domain;
+
+import com.project.tcg.domain.trade.domain.enums.EmojiType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Emoji {
+    @Id
+    @Column(name = "emoji_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 30)
+    private EmojiType emojiType;
+
+    @NotNull
+    private String emojiImageUrl;
+}
