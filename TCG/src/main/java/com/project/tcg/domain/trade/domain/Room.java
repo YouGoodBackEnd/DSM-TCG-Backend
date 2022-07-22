33f1fc65edc26ba21f3cfd71b894b7f66ddd1178
rlaisqls <rlaisqls@gmail.com>
2022-07-22 14:17:02 +0900

    📑 :: Room

---
+++ TCG/src/main/java/com/project/tcg/domain/trade/domain/Room.java
@@ -0,0 +1,37 @@
+package com.project.tcg.domain.trade.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Room {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "room")
+    private List<RoomUser> RoomUsers = new ArrayList<>();
+
+}
