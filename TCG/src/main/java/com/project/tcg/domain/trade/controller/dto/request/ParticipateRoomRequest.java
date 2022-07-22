bd86c09158cbb6b9b43a0cadb518bb5bda3a7720
rlaisqls <rlaisqls@gmail.com>
2022-07-22 14:13:46 +0900

    📑 :: ParticipateRoomRequest

---
+++ TCG/src/main/java/com/project/tcg/domain/trade/controller/dto/request/ParticipateRoomRequest.java
@@ -0,0 +1,10 @@
+package com.project.tcg.domain.trade.controller.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ParticipateRoomRequest {
+    Long roomId;
+}
