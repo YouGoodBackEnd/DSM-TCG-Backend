9677bfe320aec9b6043228c91067af0cd50bef4a
rlaisqls <rlaisqls@gmail.com>
2022-07-22 14:16:26 +0900

    ⚡ :: EmojiImageUrl 추가

--- TCG/src/main/java/com/project/tcg/domain/trade/controller/dto/response/ChatResponse.java
+++ TCG/src/main/java/com/project/tcg/domain/trade/controller/dto/response/ChatResponse.java
@@ -1,16 +1,19 @@
 package com.project.tcg.domain.trade.controller.dto.response;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.lang.Nullable;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Builder
 public class ChatResponse {
 
     private String username;
+
+    @Nullable
+    private String EmojiImageUrl;
 
     private String chat;
 }
