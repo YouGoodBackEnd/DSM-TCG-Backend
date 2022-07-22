07e9f28090ec717d2217874f8897e99eb85a9d22
rlaisqls <rlaisqls@gmail.com>
2022-07-22 14:19:14 +0900

    📑 :: RoomNotFoundExcpetion

---
+++ TCG/src/main/java/com/project/tcg/domain/trade/exception/RoomNotFoundException.java
@@ -0,0 +1,11 @@
+package com.project.tcg.domain.trade.exception;
+
+import com.project.tcg.global.error.exception.BusinessException;
+import com.project.tcg.global.error.exception.ErrorCode;
+
+public class RoomNotFoundException extends BusinessException {
+    public static final BusinessException EXCEPTION = new RoomNotFoundException();
+    private RoomNotFoundException(){
+        super(ErrorCode.ROOM_NOT_FOUND);
+    }
+}
