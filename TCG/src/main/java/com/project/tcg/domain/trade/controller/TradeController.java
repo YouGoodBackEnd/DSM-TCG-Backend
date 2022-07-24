301a60be38af49f12fc8d2d018da539d75418e32
rlaisqls <rlaisqls@gmail.com>
2022-07-24 22:55:20 +0900

    ⚡ :: CancelSuggest 추가

--- TCG/src/main/java/com/project/tcg/domain/trade/controller/TradeController.java
+++ TCG/src/main/java/com/project/tcg/domain/trade/controller/TradeController.java
@@ -2,21 +2,38 @@ package com.project.tcg.domain.trade.controller;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.annotation.OnEvent;
-import com.project.tcg.domain.trade.controller.dto.request.ChatRequest;
-import com.project.tcg.domain.trade.service.ChattingService;
-import com.project.tcg.global.websocket.property.SocketProperty;
+import com.project.tcg.domain.trade.controller.dto.request.SuggestRequest;
+import com.project.tcg.domain.trade.controller.dto.response.SuggestDto;
+import com.project.tcg.domain.trade.service.CancelAcceptService;
+import com.project.tcg.domain.trade.service.CancelSuggestService;
+import com.project.tcg.domain.trade.service.DoAcceptService;
+import com.project.tcg.domain.trade.service.DoSuggestService;
+import com.project.tcg.global.socket.annotation.SocketController;
+import com.project.tcg.global.socket.annotation.SocketMapping;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
+@SocketController
 @RequiredArgsConstructor
 @RestController
 public class TradeController {
 
-    private final ChattingService chattingService;
+    private final DoSuggestService doSuggestService;
 
-    @OnEvent(SocketProperty.CHAT)
-    public void chatting(SocketIOClient socketIOClient, SocketIOServer server, ChatRequest request) {
-        chattingService.execute(socketIOClient, server, request);
+    private final CancelSuggestService cancelSuggestService;
+
+    private final DoAcceptService doAcceptService;
+
+    private final CancelAcceptService cancelAcceptService;
+
+    @SocketMapping(endpoint = "suggest", requestCls = SuggestDto.class)
+    public void doSuggest(SocketIOClient socketIOClient, SocketIOServer server, SuggestRequest request) {
+        doSuggestService.execute(socketIOClient, server, request);
     }
+
+    @SocketMapping(endpoint = "suggest")
+    public void cancelSuggest(SocketIOClient socketIOClient, SocketIOServer server) {
+        cancelSuggestService.execute(socketIOClient, server);
+    }
+
 }
