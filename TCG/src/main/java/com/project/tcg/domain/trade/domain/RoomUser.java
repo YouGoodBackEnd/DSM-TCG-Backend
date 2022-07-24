9214f06f0c4750165ecaa50b9582ceb7f88f2fd6
rlaisqls <rlaisqls@gmail.com>
2022-07-24 22:55:34 +0900

    ⚡ :: Suggest 저장

--- TCG/src/main/java/com/project/tcg/domain/trade/domain/RoomUser.java
+++ TCG/src/main/java/com/project/tcg/domain/trade/domain/RoomUser.java
@@ -8,11 +8,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
@@ -20,6 +23,7 @@ import javax.persistence.ManyToOne;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Entity
 public class RoomUser {
     @Id
@@ -34,11 +38,22 @@ public class RoomUser {
     @Column
     private Boolean isAccept;
 
+    @Embedded
+    private Suggest suggest;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "room_id")
     private Room room;
 
     public void acceptFalse() {
         this.isAccept = false;
+    }
+
+    public void cancelSuggest() {
+        this.suggest = new Suggest();
+    }
+
+    public void setSuggest(Long suggestCardId, Integer suggestCoin) {
+        
     }
 }
