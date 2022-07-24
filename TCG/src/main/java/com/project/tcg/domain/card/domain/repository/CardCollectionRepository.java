d2c89b1596e211c0dcc7a280bc534f391ef96b7c
rlaisqls <rlaisqls@gmail.com>
2022-07-24 22:33:51 +0900

    🗑️ :: 식별관계 복합키 삭제

--- TCG/src/main/java/com/project/tcg/domain/card/domain/repository/CardCollectionRepository.java
+++ TCG/src/main/java/com/project/tcg/domain/card/domain/repository/CardCollectionRepository.java
@@ -1,12 +1,14 @@
 package com.project.tcg.domain.card.domain.repository;
 
-import com.project.tcg.domain.card.domain.CardCollection;
-import com.project.tcg.domain.card.domain.CardCollectionId;
+import com.project.tcg.domain.card.domain.UserCard;
+import com.project.tcg.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface CardCollectionRepository extends CrudRepository<CardCollection, CardCollectionId> {
-    List<CardCollection> findByUserId(Long userId);
+public interface CardCollectionRepository extends CrudRepository<UserCard, Long> {
+    List<UserCard> findByUserId(Long userId);
+    Optional<UserCard> findByUserAndCard_id(User user, Long cardId);
 
 }
