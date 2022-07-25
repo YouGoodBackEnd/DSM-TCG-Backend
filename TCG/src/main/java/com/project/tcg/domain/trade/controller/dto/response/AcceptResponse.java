7cb0499db79ddb22498bf2d7e39f8dabfba6d3d7
rlaisqls <rlaisqls@gmail.com>
2022-07-25 23:25:02 +0900

    📑 :: AccecptReponse

---
+++ TCG/src/main/java/com/project/tcg/domain/trade/controller/dto/response/AcceptResponse.java
@@ -0,0 +1,12 @@
+package com.project.tcg.domain.trade.controller.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AcceptResponse {
+
+    private Boolean isAccepted;
+
+}
