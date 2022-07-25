d53d5b3ab14f0ba76bddf938cbc17b8f83d3d48a
rlaisqls <rlaisqls@gmail.com>
2022-07-25 23:36:51 +0900

    ⚡ :: ConnectListener, DisconnectListener 추가
    @OnConnect, @OnDisconnect 어노테이션이 갑자기 안먹힘;;;; @OnEvent도 안되더니 진짜 왜 이러는지 모르겠음 에러 검색해도 방법이 안나와서 그냥 작동하는 방식으로 구현하기로했음

--- TCG/src/main/java/com/project/tcg/global/socket/config/WebSocketConfig.java
+++ TCG/src/main/java/com/project/tcg/global/socket/config/WebSocketConfig.java
@@ -4,13 +4,20 @@ import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketConfig;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
+import com.project.tcg.domain.trade.facade.RoomUserFacade;
+import com.project.tcg.domain.user.facade.UserFacade;
+import com.project.tcg.global.security.jwt.JwtTokenProvider;
 import com.project.tcg.global.socket.WebSocketAddMappingSupporter;
+import com.project.tcg.global.socket.WebSocketDisConnectListener;
+import com.project.tcg.global.socket.WebSocketOnConnectListener;
 import com.project.tcg.global.socket.exception.SocketExceptionListener;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class WebSocketConfig {
@@ -18,28 +25,35 @@ public class WebSocketConfig {
     @Value("${socket.port}")
     private Integer port;
 
-    private final WebSocketAddMappingSupporter mappingSupporter;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final UserFacade userFacade;
+
+    private final RoomUserFacade roomUserFacade;
+
+    private final WebSocketAddMappingSupporter webSocketAddMappingSupporter;
 
     @Bean
     public SocketIOServer socketIOServer() {
-
         SocketConfig socketConfig = new SocketConfig();
         socketConfig.setReuseAddress(true);
 
-        Configuration config = new Configuration();
-        config.setPort(port);
-        config.setOrigin("*");
-        config.setSocketConfig(socketConfig);
-        config.setExceptionListener(new SocketExceptionListener());
+        Configuration configuration = new Configuration();
+        configuration.setPort(port);
+        configuration.setOrigin("*");
+        configuration.setSocketConfig(socketConfig);
+        configuration.setExceptionListener(new SocketExceptionListener());
 
-        SocketIOServer server = new SocketIOServer(config);
-        mappingSupporter.addListeners(server);
-        return server;
+        SocketIOServer socketIOServer = new SocketIOServer(configuration);
+        socketIOServer.addConnectListener(new WebSocketOnConnectListener(jwtTokenProvider));
+        socketIOServer.addDisconnectListener(new WebSocketDisConnectListener(userFacade, roomUserFacade, socketIOServer));
+        webSocketAddMappingSupporter.addListeners(socketIOServer);
+        return socketIOServer;
     }
 
     @Bean
-    public SpringAnnotationScanner springAnnotationScanner() {
-        return new SpringAnnotationScanner(socketIOServer());
+    public SpringAnnotationScanner springAnnotationScanner(SocketIOServer socketIOServer) {
+        return new SpringAnnotationScanner(socketIOServer);
     }
 
 }
