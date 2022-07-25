4a5b8376b0cc0cd3a72c3f7151b3d92be21f8244
rlaisqls <rlaisqls@gmail.com>
2022-07-25 23:24:46 +0900

    📑 :: AcceptRequest

---
+++ TCG/src/main/java/com/project/tcg/domain/trade/controller/dto/request/AcceptRequest.java
@@ -0,0 +1,12 @@
+package com.project.tcg.domain.trade.controller.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AcceptRequest {
+
+    private Long roomId;
+
+}
