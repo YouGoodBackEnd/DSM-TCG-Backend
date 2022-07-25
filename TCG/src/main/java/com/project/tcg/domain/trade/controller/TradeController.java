0d68e8a3558ea6d3add3bffd4121a309c34cd3a6
rlaisqls <rlaisqls@gmail.com>
2022-07-25 23:30:55 +0900

    ♻️ :: Suggest -> Offer

--- TCG/src/main/java/com/project/tcg/domain/trade/controller/TradeController.java
+++ TCG/src/main/java/com/project/tcg/domain/trade/controller/TradeController.java
@@ -5,7 +5,7 @@ import com.corundumstudio.socketio.SocketIOServer;
 import com.project.tcg.domain.trade.controller.dto.request.AcceptRequest;
 import com.project.tcg.domain.trade.controller.dto.request.OfferRequest;
 import com.project.tcg.domain.trade.service.AcceptService;
-import com.project.tcg.domain.trade.service.SuggestService;
+import com.project.tcg.domain.trade.service.OfferService;
 import com.project.tcg.global.socket.annotation.SocketController;
 import com.project.tcg.global.socket.annotation.SocketMapping;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TradeController {
 
-    private final SuggestService suggestService;
+    private final OfferService offerService;
 
     private final AcceptService acceptService;
 
     @SocketMapping(endpoint = "suggest", requestCls = OfferRequest.class)
     public void suggest(SocketIOClient socketIOClient, SocketIOServer server, OfferRequest request) {
-        suggestService.execute(socketIOClient, server, request);
+        offerService.execute(socketIOClient, server, request);
     }
 
     @SocketMapping(endpoint = "accept", requestCls = AcceptRequest.class)
