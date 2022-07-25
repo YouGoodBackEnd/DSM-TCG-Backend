012c27082453374975e857aad0cf299df12cb322
rlaisqls <rlaisqls@gmail.com>
2022-07-25 23:28:36 +0900

    ⚡ :: emoji 전송 기능 추가

--- TCG/src/main/java/com/project/tcg/domain/chat/service/ChattingService.java
+++ TCG/src/main/java/com/project/tcg/domain/chat/service/ChattingService.java
@@ -2,11 +2,14 @@ package com.project.tcg.domain.chat.service;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.project.tcg.domain.chat.controller.dto.request.ChatRequest;
+import com.project.tcg.domain.chat.controller.dto.response.ChatResponse;
+import com.project.tcg.domain.chat.domain.enums.EmojiType;
+import com.project.tcg.domain.chat.domain.repository.EmojiRepository;
+import com.project.tcg.domain.chat.exception.EmojiNotFoundException;
 import com.project.tcg.domain.trade.domain.Room;
 import com.project.tcg.domain.trade.facade.RoomFacade;
 import com.project.tcg.domain.trade.facade.RoomUserFacade;
-import com.project.tcg.domain.chat.controller.dto.request.ChatRequest;
-import com.project.tcg.domain.chat.controller.dto.response.ChatResponse;
 import com.project.tcg.domain.user.domain.User;
 import com.project.tcg.domain.user.facade.UserFacade;
 import com.project.tcg.global.socket.SocketProperty;
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 @RequiredArgsConstructor
 @Service
@@ -24,26 +29,37 @@ public class ChattingService {
 
     private final UserFacade userFacade;
 
+    private final EmojiRepository emojiRepository;
+
     private final RoomUserFacade roomUserFacade;
 
     @Transactional
-    public void execute(SocketIOClient socketIOClient, SocketIOServer server, ChatRequest request){
+    public void execute(SocketIOClient socketIOClient, SocketIOServer socketIOServer, ChatRequest request){
 
-        Room room = roomFacade.getRoomById(Long.valueOf(request.getRoomId()));
+        Room room = roomFacade.getRoomById(request.getRoomId());
         User user = userFacade.getUserByClient(socketIOClient);
 
         roomUserFacade.CheckRoomUserIsExist(room,user);
 
+        String emojiImageUrl = getEmojiImageUrl(request.getEmojiType());
+
         ChatResponse response = ChatResponse.builder()
+                .userId(user.getId())
+                .profileImageUrl(user.getProfileImageUrl())
                 .username(user.getName())
+                .emojiImageUrl(emojiImageUrl)
                 .chat(request.getChat())
-                .sentAt(LocalDateTime.now())
+                .sentAt(LocalDateTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)))
                 .build();
 
-        server.getRoomOperations(room.getId().toString())
+        socketIOServer.getRoomOperations(room.getId().toString())
                         .sendEvent(SocketProperty.CHAT, response);
+    }
 
-        server.getRoomOperations(room.getId().toString())
-                .sendEvent(SocketProperty.CHAT, response);
+    private String getEmojiImageUrl(EmojiType emojiType) {
+        if(emojiType == null) return null;
+        else return emojiRepository.findByEmojiType(emojiType)
+                .orElseThrow(()-> EmojiNotFoundException.EXCEPTION)
+                .getEmojiImageUrl();
     }
 }
