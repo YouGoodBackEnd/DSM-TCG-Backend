2fcfb254fb599fb1a0b034622da2ea4c50039122
rlaisqls <rlaisqls@gmail.com>
2022-07-25 23:34:44 +0900

    ⚡ :: Accept가 가능한 상태인지 판별

--- TCG/src/main/java/com/project/tcg/domain/trade/service/AcceptService.java
+++ TCG/src/main/java/com/project/tcg/domain/trade/service/AcceptService.java
@@ -2,12 +2,11 @@ package com.project.tcg.domain.trade.service;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
-import com.project.tcg.domain.card.facade.UserCardFacade;
-import com.project.tcg.domain.trade.controller.dto.request.SuggestRequest;
-import com.project.tcg.domain.trade.controller.dto.response.SuggestDto;
+import com.project.tcg.domain.trade.controller.dto.request.AcceptRequest;
+import com.project.tcg.domain.trade.controller.dto.response.AcceptResponse;
 import com.project.tcg.domain.trade.domain.Room;
 import com.project.tcg.domain.trade.domain.RoomUser;
-import com.project.tcg.domain.trade.domain.repository.RoomUserRepository;
+import com.project.tcg.domain.trade.exception.DidNotOfferedException;
 import com.project.tcg.domain.trade.facade.RoomFacade;
 import com.project.tcg.domain.trade.facade.RoomUserFacade;
 import com.project.tcg.domain.user.domain.User;
@@ -15,36 +14,49 @@ import com.project.tcg.domain.user.facade.UserFacade;
 import com.project.tcg.global.socket.SocketProperty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
-public class CancelSuggestService {
+public class AcceptService {
+
     private final RoomFacade roomFacade;
 
     private final UserFacade userFacade;
 
     private final RoomUserFacade roomUserFacade;
 
-    private final UserCardFacade userCardFacade;
-
-    private final RoomUserRepository roomUserRepository;
-
-    public void execute(SocketIOClient socketIOClient, SocketIOServer server, SuggestRequest request) {
+    @Transactional
+    public void execute(SocketIOClient socketIOClient, SocketIOServer server, AcceptRequest request) {
 
         Room room = roomFacade.getRoomById(request.getRoomId());
         User user = userFacade.getUserByClient(socketIOClient);
 
         RoomUser roomUser = roomUserFacade.getRoomUserByRoomAndUser(room, user);
 
-        roomUser.cancelSuggest();
+        if (!room.checkBothOffered() || room.getRoomUsers().size() < 2) {
+            throw DidNotOfferedException.EXCEPTION;
+        }
 
-        SuggestDto response = SuggestDto
-                .builder()
-                .cardId(null)
-                .coin(null)
-                .build();
+        if (roomUser.getIsAccepted()) roomUser.cancelAccept();
+        else roomUser.doAccept();
+        AcceptResponse response = new AcceptResponse(roomUser.getIsAccepted());
 
         server.getRoomOperations(room.getId().toString())
                 .sendEvent(SocketProperty.SUGGEST, response);
+
+        if (isTradeable(room)) {
+            doTrade(room);
+            server.getRoomOperations(room.getId().toString())
+                    .sendEvent(SocketProperty.TRADE);
+        }
+    }
+
+    private boolean isTradeable(Room room) {
+        return room.checkBothAccepted();
+    }
+
+    private void doTrade(Room room) {
+
     }
 }
