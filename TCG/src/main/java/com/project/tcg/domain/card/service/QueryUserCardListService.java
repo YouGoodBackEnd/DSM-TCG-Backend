1e97c964f80261d5bf76e6729a3c961874150b03
rlaisqls <rlaisqls@gmail.com>
2022-07-25 23:19:19 +0900

    ♻️ :: 안쓰는 의존성 삭제

--- TCG/src/main/java/com/project/tcg/domain/card/service/QueryUserCardListService.java
+++ TCG/src/main/java/com/project/tcg/domain/card/service/QueryUserCardListService.java
@@ -1,10 +1,8 @@
 package com.project.tcg.domain.card.service;
 
-import com.project.tcg.domain.card.domain.repository.CardRepository;
 import com.project.tcg.domain.card.facade.UserCardFacade;
 import com.project.tcg.domain.card.presentation.dto.response.CardInfoResponse;
 import com.project.tcg.domain.card.presentation.dto.response.CardListResponse;
-import com.project.tcg.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,11 +13,7 @@ import java.util.stream.Collectors;
 @Service
 public class QueryUserCardListService {
 
-    private final CardRepository cardRepository;
-
     private final UserCardFacade userCardFacade;
-
-    private final UserFacade userFacade;
 
     public CardListResponse execute(Long userId) {
 
