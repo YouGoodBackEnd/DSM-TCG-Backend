0639179a6c0c61190e45d3d2c61bd62053d70b9b
rlaisqls <rlaisqls@gmail.com>
2022-07-26 13:30:42 +0900

    ⚡ :: findById 추가

--- TCG/src/main/java/com/project/tcg/domain/card/domain/repository/CardRepository.java
+++ TCG/src/main/java/com/project/tcg/domain/card/domain/repository/CardRepository.java
@@ -4,8 +4,11 @@ import com.project.tcg.domain.card.domain.Card;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CardRepository extends CrudRepository<Card, Long> {
 
     List<Card> findBy();
+
+    Optional<Card> findById(Long id);
 }
