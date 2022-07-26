a8fa0963e59f9694c1fb181b2a23a77b39d34b2b
rlaisqls <rlaisqls@gmail.com>
2022-07-26 11:34:50 +0900

    ⚡ :: Trade, Accept 이벤트 추가

--- TCG/src/main/java/com/project/tcg/global/socket/SocketProperty.java
+++ TCG/src/main/java/com/project/tcg/global/socket/SocketProperty.java
@@ -6,13 +6,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SocketProperty {
 
-    public static final String SUGGEST = "suggest";
-
     public static final String CHAT = "chat";
 
     public static final String ROOM = "room";
 
-    public static final String TRADE = "Trade";
+    public static final String TRADE = "trade";
+
+    public static final String ACCEPT = "accept";
+
+    public static final String OFFER = "offer";
 
     public static final String ERROR = "error";
+
+
 }
