a69dd49a6e10e95e7becba65ff2306d23b13b5e6
rlaisqls <rlaisqls@gmail.com>
2022-07-26 11:32:59 +0900

    ⚡ :: Trade 구현

--- TCG/src/main/java/com/project/tcg/domain/trade/service/AcceptService.java
+++ TCG/src/main/java/com/project/tcg/domain/trade/service/AcceptService.java
@@ -2,10 +2,15 @@ package com.project.tcg.domain.trade.service;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.project.tcg.domain.card.domain.Card;
+import com.project.tcg.domain.card.facade.CardFacade;
 import com.project.tcg.domain.trade.controller.dto.request.AcceptRequest;
 import com.project.tcg.domain.trade.controller.dto.response.AcceptResponse;
+import com.project.tcg.domain.trade.controller.dto.response.TradeResponse;
+import com.project.tcg.domain.trade.domain.Offer;
 import com.project.tcg.domain.trade.domain.Room;
 import com.project.tcg.domain.trade.domain.RoomUser;
+import com.project.tcg.domain.trade.domain.repository.RoomUserRepository;
 import com.project.tcg.domain.trade.exception.DidNotOfferedException;
 import com.project.tcg.domain.trade.facade.RoomFacade;
 import com.project.tcg.domain.trade.facade.RoomUserFacade;
@@ -26,6 +31,10 @@ public class AcceptService {
 
     private final RoomUserFacade roomUserFacade;
 
+    private final CardFacade cardFacade;
+
+    private final RoomUserRepository roomUserRepository;
+
     @Transactional
     public void execute(SocketIOClient socketIOClient, SocketIOServer server, AcceptRequest request) {
 
@@ -40,15 +49,17 @@ public class AcceptService {
 
         if (roomUser.getIsAccepted()) roomUser.cancelAccept();
         else roomUser.doAccept();
+        roomUserRepository.save(roomUser);
+
         AcceptResponse response = new AcceptResponse(roomUser.getIsAccepted());
 
         server.getRoomOperations(room.getId().toString())
-                .sendEvent(SocketProperty.SUGGEST, response);
+                .sendEvent(SocketProperty.ACCEPT, response);
 
         if (isTradeable(room)) {
             doTrade(room);
             server.getRoomOperations(room.getId().toString())
-                    .sendEvent(SocketProperty.TRADE);
+                    .sendEvent(SocketProperty.TRADE, new TradeResponse("거래가 완료됐습니다"));
         }
     }
 
@@ -58,5 +69,18 @@ public class AcceptService {
 
     private void doTrade(Room room) {
 
+        RoomUser roomUser1 = room.getRoomUsers().get(0);
+        User user1 = roomUser1.getUser();
+        Offer offer1 = roomUser1.getOffer();
+        Card offerCard1 = cardFacade.getCardById(offer1.getCardId());
+
+        RoomUser roomUser2 = room.getRoomUsers().get(1);
+        User user2 = roomUser2.getUser();
+        Offer offer2 = roomUser2.getOffer();
+        Card offerCard2 = cardFacade.getCardById(offer2.getCardId());
+
+        user1.giveResourcesToUser(offerCard1, offer1.getCoin(), user2);
+        user2.giveResourcesToUser(offerCard2, offer2.getCoin(), user1);
     }
+
 }
