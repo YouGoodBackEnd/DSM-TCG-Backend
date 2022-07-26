9f3bd663286813d8363ca59e7ee0278c23390738
rlaisqls <rlaisqls@gmail.com>
2022-07-26 11:32:13 +0900

    📑 :: TradeResponse

---
+++ TCG/src/main/java/com/project/tcg/domain/trade/controller/dto/response/TradeResponse.java
@@ -0,0 +1,11 @@
+package com.project.tcg.domain.trade.controller.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TradeResponse {
+
+    private String notification;
+}
