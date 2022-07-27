74ae7ce809d66d9154f9c38c4defd3ffc8850165
rlaisqls <rlaisqls@gmail.com>
2022-07-27 11:22:51 +0900

    ⚡ :: getUserCardCount 추가

--- TCG/src/main/java/com/project/tcg/domain/card/facade/UserCardFacade.java
+++ TCG/src/main/java/com/project/tcg/domain/card/facade/UserCardFacade.java
@@ -1,32 +1,20 @@
 package com.project.tcg.domain.card.facade;
 
-import com.project.tcg.domain.card.domain.Card;
-import com.project.tcg.domain.card.domain.UserCard;
-import com.project.tcg.domain.card.domain.repository.CardCollectionRepository;
+import com.project.tcg.domain.card.domain.repository.UserCardRepository;
 import com.project.tcg.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
 public class UserCardFacade {
 
-    private final CardCollectionRepository cardCollectionRepository;
-
-    public List<Card> getUserCardList(Long userId) {
-
-        return cardCollectionRepository.findByUserId(userId)
-                .stream()
-                .map(UserCard::getCard)
-                .collect(Collectors.toList());
-    }
-
+    private final UserCardRepository userCardRepository;
     public Boolean checkUserCardExist(User user, Long cardId){
-
-        return cardCollectionRepository.findByUserAndCard_id(user,cardId).isPresent();
+        return userCardRepository.findFirstByUserAndCard_id(user,cardId).isPresent();
     }
 
+    public int getUserCardCount(Long cardId, User user) {
+        return userCardRepository.findByCard_IdAndUser(cardId, user).size();
+    }
 }
