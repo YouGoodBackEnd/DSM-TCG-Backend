4b2953ec67103aef718dd0bc49629857c3b4cf50
rlaisqls <rlaisqls@gmail.com>
2022-07-27 11:28:34 +0900

    📑 :: AlreadyAcceptedException

---
+++ TCG/src/main/java/com/project/tcg/domain/trade/exception/AlreadyAcceptedException.java
@@ -0,0 +1,12 @@
+package com.project.tcg.domain.trade.exception;
+
+import com.project.tcg.global.error.exception.BusinessException;
+import com.project.tcg.global.error.exception.ErrorCode;
+
+public class AlreadyAcceptedException extends BusinessException {
+    public static final BusinessException EXCEPTION = new AlreadyAcceptedException();
+
+    private AlreadyAcceptedException() {
+        super(ErrorCode.ALREADY_ACCEPTED);
+    }
+}
