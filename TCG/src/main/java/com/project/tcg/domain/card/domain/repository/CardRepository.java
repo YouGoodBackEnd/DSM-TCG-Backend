c75843647677ac05dbc89b3930af72e131b2a424
rlaisqls <rlaisqls@gmail.com>
2022-07-27 11:21:18 +0900

    ⚡ :: findByGrade 추가

--- TCG/src/main/java/com/project/tcg/domain/card/domain/repository/CardRepository.java
+++ TCG/src/main/java/com/project/tcg/domain/card/domain/repository/CardRepository.java
@@ -1,6 +1,7 @@
 package com.project.tcg.domain.card.domain.repository;
 
 import com.project.tcg.domain.card.domain.Card;
+import com.project.tcg.domain.card.domain.Grade;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.Optional;
 
 public interface CardRepository extends CrudRepository<Card, Long> {
 
-    List<Card> findBy();
+    List<Card> findByGrade(Grade grade);
 
     Optional<Card> findById(Long id);
 }
