8067e06d306ebc00787ec65a88396ccbbfc5c03f
rlaisqls <rlaisqls@gmail.com>
2022-07-27 11:18:10 +0900

    📑 :: DrawChestService들들들

---
+++ TCG/src/main/java/com/project/tcg/domain/chest/service/DrawLegendChestService.java
@@ -0,0 +1,25 @@
+package com.project.tcg.domain.chest.service;
+
+import com.project.tcg.domain.chest.Facade.ChestFacade;
+import com.project.tcg.domain.chest.domain.DrawProbability;
+import com.project.tcg.domain.chest.presentation.dto.response.DrawChestResponse;
+import com.project.tcg.domain.user.domain.User;
+import com.project.tcg.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DrawLegendChestService {
+
+    private final ChestFacade chestFacade;
+
+    private final UserFacade userFacade;
+
+    public DrawChestResponse execute() {
+
+        User user = userFacade.getCurrentUser();
+
+        return chestFacade.getDrawChestResponse(user, 3, DrawProbability.LEGEND_CHEST);
+    }
+}
