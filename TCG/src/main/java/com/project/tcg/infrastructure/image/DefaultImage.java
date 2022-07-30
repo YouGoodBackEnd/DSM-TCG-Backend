c179e77f2bd652af0d4da9ca69d832b4378de12e
rlaisqls <rlaisqls@gmail.com>
2022-07-30 12:09:52 +0900

    🐛 ::  따옴표 삭제

--- TCG/src/main/java/com/project/tcg/infrastructure/image/DefaultImage.java
+++ TCG/src/main/java/com/project/tcg/infrastructure/image/DefaultImage.java
@@ -4,5 +4,5 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class DefaultImage {
-    public static final String USER_PROFILE_IMAGE = "\"https://ssl.pstatic.net/static/blog/img_profile_default.png?type=s1\"";
+    public static final String USER_PROFILE_IMAGE = "https://ssl.pstatic.net/static/blog/img_profile_default.png?type=s1";
 }
