66062bab8b1a13e7459337a3426b2ea3d37c0015
rlaisqls <rlaisqls@gmail.com>
2022-08-03 11:26:42 +0900

    📑 :: QuerySpecialChestOpenDateTimeService

---
+++ TCG/src/main/java/com/project/tcg/domain/chest/service/QuerySpecialChestOpenDateTimeService.java
@@ -0,0 +1,32 @@
+package com.project.tcg.domain.chest.service;
+
+import com.project.tcg.domain.chest.Facade.UserChestFacade;
+import com.project.tcg.domain.chest.presentation.dto.response.QueryChestOpenDateTimeResponse;
+import com.project.tcg.domain.user.domain.User;
+import com.project.tcg.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class QuerySpecialChestOpenDateTimeService {
+
+    private final UserFacade userFacade;
+    private final UserChestFacade userChestFacade;
+
+    public QueryChestOpenDateTimeResponse execute() {
+
+        User user = userFacade.getCurrentUser();
+
+        LocalDateTime openDateTime = userChestFacade.getUserChestById(user)
+                .getSpecialChestOpenDateTime();
+
+        return QueryChestOpenDateTimeResponse
+                .builder()
+                .chestOpenDateTime(openDateTime)
+                .isOpened(openDateTime.isAfter(LocalDateTime.now()))
+                .build();
+    }
+}
