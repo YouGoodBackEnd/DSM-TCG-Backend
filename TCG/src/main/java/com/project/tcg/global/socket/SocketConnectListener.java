aa448bf64a2e3dcbc8fd03b0fffe68aba78b8e24
rlaisqls <rlaisqls@gmail.com>
2022-08-03 11:29:10 +0900

    ⚡ :: @OnConnect, @OnDisConnect 사용

--- TCG/src/main/java/com/project/tcg/global/socket/SocketConnectListener.java
+++ TCG/src/main/java/com/project/tcg/global/socket/SocketConnectListener.java
@@ -2,17 +2,23 @@ package com.project.tcg.global.socket;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.listener.DisconnectListener;
+import com.corundumstudio.socketio.annotation.OnConnect;
+import com.corundumstudio.socketio.annotation.OnDisconnect;
 import com.project.tcg.domain.chat.controller.dto.response.RoomNotificationResponse;
 import com.project.tcg.domain.trade.facade.RoomUserFacade;
 import com.project.tcg.domain.user.domain.User;
 import com.project.tcg.domain.user.facade.UserFacade;
+import com.project.tcg.global.security.jwt.JwtTokenProvider;
+import com.project.tcg.global.socket.sercurity.ClientProperty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class WebSocketDisConnectListener implements DisconnectListener {
+public class SocketConnectListener {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     private final UserFacade userFacade;
 
@@ -20,7 +26,17 @@ public class WebSocketDisConnectListener implements DisconnectListener {
 
     private final SocketIOServer socketIOServer;
 
-    @Override
+    @OnConnect
+    public void onConnect(SocketIOClient socketIOClient) {
+
+        String token = socketIOClient.getHandshakeData().getHttpHeaders().get("Authorization");
+        Authentication authentication = jwtTokenProvider.getAuthentication(token);
+
+        String accountId = authentication.getName();
+        socketIOClient.set(ClientProperty.USER_KEY, accountId);
+    }
+
+    @OnDisconnect
     public void onDisconnect(SocketIOClient socketIOClient) {
 
         User user = userFacade.getUserByClient(socketIOClient);
