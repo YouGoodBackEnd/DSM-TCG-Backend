eadd8dcee3ea344a7c77964790eabd843e63fff1
rlaisqls <rlaisqls@gmail.com>
2022-08-05 13:08:43 +0900

    ⚡ :: ID를 UserId로 매핑

--- src/main/java/com/project/tcg/domain/chat/domain/RoomUser.java
+++ src/main/java/com/project/tcg/domain/chat/domain/RoomUser.java
@@ -12,13 +12,14 @@ import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Builder
@@ -27,19 +28,20 @@ import javax.persistence.ManyToOne;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Entity
 public class RoomUser {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
+    @Column(name = "user_id", nullable = false)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Column
+    @NotNull
     private Boolean isAccepted;
 
-    @Column
+    @NotNull
     private Boolean isOffered;
 
     @Embedded
