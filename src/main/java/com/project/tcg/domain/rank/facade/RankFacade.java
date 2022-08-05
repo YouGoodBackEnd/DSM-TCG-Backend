74a2dd41833b61665be1f4800c60616611c4e3fa
rlaisqls <rlaisqls@gmail.com>
2022-08-05 13:21:03 +0900

    📑 :: RankFacade

---
+++ src/main/java/com/project/tcg/domain/rank/facade/RankFacade.java
@@ -0,0 +1,37 @@
+package com.project.tcg.domain.rank.facade;
+
+import com.project.tcg.domain.rank.domain.Rank;
+import com.project.tcg.domain.rank.domain.RankId;
+import com.project.tcg.domain.rank.domain.repository.RankRepository;
+import com.project.tcg.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Component
+public class RankFacade {
+
+    private final RankRepository rankRepository;
+
+    public Long getUserRanking(User user) {
+
+        Rank rank = getRank(user);
+        return rank != null ? rank.getRanking() : null;
+    }
+
+    public Rank getRank(User user) {
+
+        return rankRepository.findById(
+                RankId
+                        .builder()
+                        .userId(user.getId())
+                        .createdAt(LocalDateTime.now()
+                                .minusHours(LocalDateTime.now().getHour())
+                                .minusMinutes(LocalDateTime.now().getMinute()))
+                        .build()
+        ).orElse(null);
+    }
+
+}
