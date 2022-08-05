544368f3b53f6bfef64552e79dce91ddd10af272
rlaisqls <rlaisqls@gmail.com>
2022-08-05 13:19:50 +0900

    ⚡ :: SELECT cardCount 추가

--- src/main/resources/createProcedures.sql
+++ src/main/resources/createProcedures.sql
@@ -1,17 +1,24 @@
-CREATE PROCEDURE SAVE_USER_RANK()
+CREATE PROCEDURE SAVE_RANK()
 BEGIN
-    INSERT INTO user_rank(
-        created_at, user_id, account_id, name, profile_image_url, ranking
+    INSERT INTO ranks(
+        created_at, user_id, account_id, name, profile_image_url, ranking,
+        ss_grade_card_count, s_grade_card_count, a_grade_card_count, b_grade_card_count, c_grade_card_count
     ) SELECT
-        CONVERT(date,GETDATE()) AS created_at,
-        user.user_id AS user_id,
-        user.account_id AS account_id,
-        user.name AS name,
-        user.profile_image_url AS profile_image_url,
-        ROW_NUMBER() OVER (ORDER BY user.ss_grade_caard_count DESC,
-                                    user.s_grade_caard_count DESC,
-                                    user.a_grade_caard_count DESC,
-                                    user.b_grade_caard_count DESC,
-                                    user.c_grade_caard_count DESC) AS ranking
-    FROM user
+          DATE_SUB(DATE_SUB(now(), interval(SECOND(now())) SECOND), interval(MINUTE(now())) MINUTE) AS created_at,
+          user.user_id,
+          user.account_id ,
+          user.name,
+          user.profile_image_url AS profile_image_url,
+          ROW_NUMBER() OVER (ORDER BY user.ss_grade_card_count DESC,
+                                        user.s_grade_card_count DESC,
+                                        user.a_grade_card_count DESC,
+                                        user.b_grade_card_count DESC,
+                                        user.c_grade_card_count DESC) AS ranking,
+          user.ss_grade_card_count,
+          user.s_grade_card_count,
+          user.a_grade_card_count,
+          user.b_grade_card_count,
+          user.c_grade_card_count
+
+    FROM user;
 END$$
