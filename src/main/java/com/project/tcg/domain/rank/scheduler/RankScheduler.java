f6f548d1fcf5c8115a91ac1f59e171e571464b09
rlaisqls <rlaisqls@gmail.com>
2022-08-05 13:21:57 +0900

    📑 :: RankScheduler

---
+++ src/main/java/com/project/tcg/domain/rank/scheduler/RankScheduler.java
@@ -0,0 +1,19 @@
+package com.project.tcg.domain.rank.scheduler;
+
+import com.project.tcg.domain.rank.domain.repository.RankRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class RankScheduler {
+
+    private final RankRepository rankRepository;
+
+    @Scheduled(cron = "0 0 0/1 * * *", zone = "Asia/Seoul")
+    public void saveRank() {
+        rankRepository.saveRank();
+    }
+
+}
