2c8b4c93ad26316dd7dc4a018b4a2a18442341f8
rlaisqls <rlaisqls@gmail.com>
2022-08-06 00:30:58 +0900

    Auto stash before rebase of "refs/heads/master"

---
+++ src/main/resources/createCard.sql
@@ -0,0 +1,24 @@
+INSERT INTO card
+(card_id, card_image_url, code, description, grade, name)
+VALUES
+    (1, 'https://myawsbucketswg1214.s3.amazonaws.com/myawsbucketswg1214/9299b688-d926-488d-97cc-4d1571a24a45SS.png', 'SS_GRADE_CARD', 'SS급 카드입니다', 'SS', 'SS급카드');
+
+INSERT INTO card
+(card_id, card_image_url, code, description, grade, name)
+VALUES
+    (2, 'https://myawsbucketswg1214.s3.amazonaws.com/myawsbucketswg1214/ed68701d-a9bc-4c23-8019-d64bb67d91c6S.png', 'S_GRADE_CARD', 'S급 카드입니다', 'S', 'S급카드');
+
+INSERT INTO card
+(card_id, card_image_url, code, description, grade, name)
+VALUES
+    (3, 'https://myawsbucketswg1214.s3.amazonaws.com/myawsbucketswg1214/a0724116-92b5-478a-8cb1-74005162ed31A.png', 'A_GRADE_CARD', 'A급 카드입니다', 'A', 'A급카드');
+
+INSERT INTO card
+(card_id, card_image_url, code, description, grade, name)
+VALUES
+    (4, 'https://myawsbucketswg1214.s3.amazonaws.com/myawsbucketswg1214/e09657e6-5504-465d-a56f-63a895262aabB.png', 'B_GRADE_CARD', 'B급 카드입니다', 'B', 'B급카드');
+
+INSERT INTO card
+(card_id, card_image_url, code, description, grade, name)
+VALUES
+    (5, 'https://myawsbucketswg1214.s3.amazonaws.com/myawsbucketswg1214/311c7691-7aa8-4d58-80b4-91eb9fd0ef2fC.png', 'C_GRADE_CARD', 'C급 카드입니다', 'C', 'C급카드');
