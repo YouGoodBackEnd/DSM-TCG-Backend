bcf4868fbebd0605aab0cdf27b392095163f96ff
rlaisqls <rlaisqls@gmail.com>
2022-08-05 13:26:56 +0900

    ♻️ :: 패키지 이동

--- src/main/java/com/project/tcg/global/config/WebConfig.java
+++ src/main/java/com/project/tcg/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.project.tcg.global.socket.config;
+package com.project.tcg.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
