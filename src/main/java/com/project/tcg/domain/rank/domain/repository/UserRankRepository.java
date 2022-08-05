045c739467befb79c26e4ace79adfd2c0e0071f3
rlaisqls <rlaisqls@gmail.com>
2022-08-05 13:18:59 +0900

    🗑️ :: UserRankFacade, Repository

--- src/main/java/com/project/tcg/domain/rank/domain/repository/UserRankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.tcg.domain.rank.domain.repository;
-
-import com.project.tcg.domain.rank.domain.UserRank;
-import com.project.tcg.domain.rank.domain.UserRankId;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRankRepository extends CrudRepository<UserRank, UserRankId> {
-
-}
