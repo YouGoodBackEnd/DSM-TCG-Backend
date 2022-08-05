9be91eb7d6b3a47aa989ebc790b74bd7836bd08c
rlaisqls <rlaisqls@gmail.com>
2022-08-06 00:12:18 +0900

    ⚡ :: EnumType.STRING

--- src/main/java/com/project/tcg/domain/chat/domain/Emoji.java
+++ src/main/java/com/project/tcg/domain/chat/domain/Emoji.java
@@ -7,11 +7,12 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -23,8 +24,8 @@ public class Emoji {
     private Long id;
 
     @NotNull
-    @Size(max = 30)
-    @Column(unique = true)
+    @Enumerated(EnumType.STRING)
+    @Column(length = 30, unique = true)
     private EmojiType emojiType;
 
     @NotNull
