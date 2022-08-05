49182812235a5e9a0921f3c2a3a8855e39ccc89c
rlaisqls <rlaisqls@gmail.com>
2022-08-05 13:26:43 +0900

    🐛 :: @Confiquration

--- src/main/java/com/project/tcg/global/config/WebSocketConfig.java
+++ src/main/java/com/project/tcg/global/config/WebSocketConfig.java
@@ -1,39 +1,28 @@
-package com.project.tcg.global.socket.config;
+package com.project.tcg.global.config;
 
-import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketConfig;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
-import com.project.tcg.domain.trade.facade.RoomUserFacade;
-import com.project.tcg.domain.user.facade.UserFacade;
-import com.project.tcg.global.security.jwt.JwtTokenProvider;
 import com.project.tcg.global.socket.exception.SocketExceptionListener;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Slf4j
-@Component
+@Configuration
 @RequiredArgsConstructor
 public class WebSocketConfig {
 
     @Value("${socket.port}")
     private Integer port;
 
-    private final JwtTokenProvider jwtTokenProvider;
-
-    private final UserFacade userFacade;
-
-    private final RoomUserFacade roomUserFacade;
-
     @Bean
     public SocketIOServer socketIOServer() {
+        System.out.println("WebSocketConfig.socketIOServer");
         SocketConfig socketConfig = new SocketConfig();
         socketConfig.setReuseAddress(true);
 
-        Configuration configuration = new Configuration();
+        com.corundumstudio.socketio.Configuration configuration = new com.corundumstudio.socketio.Configuration();
         configuration.setPort(port);
         configuration.setOrigin("*");
         configuration.setSocketConfig(socketConfig);
