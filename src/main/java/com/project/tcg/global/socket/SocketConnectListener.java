dca2d151077d85ac1cb5143ccc4c9c1afbd5b92c
rlaisqls <rlaisqls@gmail.com>
2022-08-05 13:20:46 +0900

    ⚡ :: OnDisconnect 이벤트 삭제

--- src/main/java/com/project/tcg/global/socket/SocketConnectListener.java
+++ src/main/java/com/project/tcg/global/socket/SocketConnectListener.java
@@ -3,10 +3,7 @@ package com.project.tcg.global.socket;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.annotation.OnConnect;
-import com.corundumstudio.socketio.annotation.OnDisconnect;
-import com.project.tcg.domain.chat.presentation.dto.response.RoomNotificationResponse;
-import com.project.tcg.domain.trade.facade.RoomUserFacade;
-import com.project.tcg.domain.user.domain.User;
+import com.project.tcg.domain.chat.facade.RoomUserFacade;
 import com.project.tcg.domain.user.facade.UserFacade;
 import com.project.tcg.global.security.jwt.JwtTokenProvider;
 import com.project.tcg.global.socket.sercurity.ClientProperty;
@@ -34,26 +31,5 @@ public class SocketConnectListener {
 
         String accountId = authentication.getName();
         socketIOClient.set(ClientProperty.USER_KEY, accountId);
-    }
-
-    @OnDisconnect
-    public void onDisconnect(SocketIOClient socketIOClient) {
-
-        User user = userFacade.getUserByClient(socketIOClient);
-
-        roomUserFacade.removeParticipatingRooms(user);
-
-        socketIOClient
-                .getAllRooms()
-                .forEach(room -> {
-
-                    RoomNotificationResponse response =
-                            new RoomNotificationResponse(Long.valueOf(room), user.getName() + "님이 나가셨습니다.");
-
-                    socketIOServer.getRoomOperations(room)
-                            .sendEvent(SocketProperty.ROOM, response);
-
-                    socketIOClient.leaveRoom(room);
-                });
     }
 }
