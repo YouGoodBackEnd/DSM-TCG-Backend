5d8c619b58612fd3227f38db5bc52d3be0c04bc8
rlaisqls <rlaisqls@gmail.com>
2022-08-06 00:12:59 +0900

    📑 :: RoomUserAlreadyExistExcception

---
+++ src/main/java/com/project/tcg/domain/chat/exception/RoomUserAlreadyExistException.java
@@ -0,0 +1,11 @@
+package com.project.tcg.domain.chat.exception;
+
+import com.project.tcg.global.error.exception.BusinessException;
+import com.project.tcg.global.error.exception.ErrorCode;
+
+public class RoomUserAlreadyExistException extends BusinessException {
+    public static final BusinessException EXCEPTION = new RoomUserAlreadyExistException();
+    private RoomUserAlreadyExistException(){
+        super(ErrorCode.ROOM_USER_ALREADY_EXIST);
+    }
+}
