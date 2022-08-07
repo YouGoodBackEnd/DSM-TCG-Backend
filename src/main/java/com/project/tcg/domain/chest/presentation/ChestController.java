be455ce735deb733a8d584fafb68d4edcda27c92
rlaisqls <rlaisqls@gmail.com>
2022-08-07 13:34:33 +0900

    ⚡ :: /chests로 변경

--- src/main/java/com/project/tcg/domain/chest/presentation/ChestController.java
+++ src/main/java/com/project/tcg/domain/chest/presentation/ChestController.java
@@ -1,18 +1,22 @@
 package com.project.tcg.domain.chest.presentation;
 
 import com.project.tcg.domain.chest.presentation.dto.response.DrawChestResponse;
+import com.project.tcg.domain.chest.presentation.dto.response.QueryChestOpenDateTimeResponse;
 import com.project.tcg.domain.chest.service.DrawFreeChestService;
 import com.project.tcg.domain.chest.service.DrawGoldChestService;
 import com.project.tcg.domain.chest.service.DrawLegendChestService;
 import com.project.tcg.domain.chest.service.DrawSilverChestService;
 import com.project.tcg.domain.chest.service.DrawSpecialChestService;
+import com.project.tcg.domain.chest.service.QueryFreeChestOpenDateTimeService;
+import com.project.tcg.domain.chest.service.QuerySpecialChestOpenDateTimeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/cards")
+@RequestMapping("/chests")
 @RestController
 public class ChestController {
 
@@ -21,28 +25,40 @@ public class ChestController {
     private final DrawSilverChestService drawSilverChestService;
     private final DrawGoldChestService drawGoldChestService;
     private final DrawLegendChestService drawLegendChestService;
+    private final QueryFreeChestOpenDateTimeService queryFreeChestOpenDateTimeService;
+    private final QuerySpecialChestOpenDateTimeService querySpecialChestOpenDateTimeService;
 
-    @PostMapping("/draw/free")
+    @PostMapping("/free")
     public DrawChestResponse drawFreeChest(){
         return drawFreeChestService.execute();
     }
 
-    @PostMapping("/draw/special")
-    public DrawChestResponse drawSpecialChest(){
+    @GetMapping("/free")
+    public QueryChestOpenDateTimeResponse queryFreeChestOpenDateTime(){
+        return queryFreeChestOpenDateTimeService.execute();
+    }
+
+    @PostMapping("/special")
+    public DrawChestResponse querySpecialChestOpenDateTime(){
         return drawSpecialChestService.execute();
     }
 
-    @PostMapping("/draw/silver")
+    @GetMapping("/special")
+    public QueryChestOpenDateTimeResponse drawSpecialChest(){
+        return querySpecialChestOpenDateTimeService.execute();
+    }
+
+    @PostMapping("/silver")
     public DrawChestResponse drawSilverChest(){
         return drawSilverChestService.execute();
     }
 
-    @PostMapping("/draw/gold")
+    @PostMapping("/gold")
     public DrawChestResponse drawGoldChest(){
         return drawGoldChestService.execute();
     }
 
-    @PostMapping("/draw/legend")
+    @PostMapping("/legend")
     public DrawChestResponse drawLegendChest(){
         return drawLegendChestService.execute();
     }
