bc99a9ae0c5c5fbd804f20106bd1b6eb8b1ce567
rlaisqls <rlaisqls@gmail.com>
2022-08-07 13:46:15 +0900

    ⚡ :: findByToken()

--- src/main/java/com/project/tcg/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/project/tcg/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package com.project.tcg.domain.auth.domain.repository;
 import com.project.tcg.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String refreshToken);
 }
