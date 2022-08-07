045e168455b714c9345e5594dfa5db68b95c3184
rlaisqls <rlaisqls@gmail.com>
2022-08-07 13:42:43 +0900

    ⚡ :: Code 수정

--- src/main/java/com/project/tcg/global/error/exception/ErrorCode.java
+++ src/main/java/com/project/tcg/global/error/exception/ErrorCode.java
@@ -21,18 +21,17 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "USER-404-2", "User Not Found" ),
     USER_ALREADY_EXIST(409, "USER-409-1", "User Already Exist"),
 
-    CARD_NOT_FOUND(404, "CARD-404-1", "Card Not Found"),
-
+    EMOJI_NOT_FOUND(404, "CHAT-404-1", "Emoji Not Found"),
     ROOM_NOT_FOUND(404, "ROOM-404-1", "Room Not Found"),
     OVERSTAFFED_ROOM(409, "ROOM-409-1", "Overstaffed Room"),
     ROOM_USER_ALREADY_EXIST(409, "ROOM-409-2", "Already joining another room"),
 
-    EMOJI_NOT_FOUND(404, "CHAT-404-1", "Emoji Not Found"),
-
-    DID_NOT_OFFERED(400, "TRADE-400-1", "Did Not Offered"),
-    COIN_LACK(409, "TRADE-409-2", "Coin Lack"),
-    CARD_LACK(409, "TRADE-409-3", "Card Lack"),
-    ALREADY_ACCEPTED(409, "TRADE-409-4", "Already Accepted"),
+    BAD_OFFER(400, "TRADE-400-1", "Bad Offer"),
+    CARD_NOT_FOUND(404, "TRADE-404-1", "Card Not Found"),
+    COIN_LACK(409, "TRADE-409-1", "Coin Lack"),
+    CARD_LACK(409, "TRADE-409-2", "Card Lack"),
+    OFFER_IMPOSSIBLE(409, "TRADE-409-3", "Offer Impossible"),
+    ACCEPT_IMPOSSIBLE(409, "TRADE-409-4", "Accept Impossible"),
 
     REFRESH_TOKEN_NOT_FOUND(404, "AUTH-404-1", "Refresh Token Not Found"),
 
