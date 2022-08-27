b7e2fefc0fbe89ca90e5c3d904b9d1baddba8311
rlaisqls <rlaisqls@gmail.com>
2022-08-27 09:44:54 +0900

    ⚡ :: CheckIsNotFulledRoom() 삭제

--- src/main/java/com/project/tcg/domain/chat/facade/RoomFacade.java
+++ src/main/java/com/project/tcg/domain/chat/facade/RoomFacade.java
@@ -2,7 +2,7 @@ package com.project.tcg.domain.chat.facade;
 
 import com.project.tcg.domain.chat.domain.Room;
 import com.project.tcg.domain.chat.domain.repository.RoomRepository;
-import com.project.tcg.domain.chat.exception.OverstaffedRoomException;
+import com.project.tcg.domain.chat.exception.FulledRoomException;
 import com.project.tcg.domain.chat.exception.RoomNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -25,11 +25,6 @@ public class RoomFacade {
             return false;
         }
         return true;
-    }
-
-    public void checkIsNotFulledRoom(Room room) {
-        if (!isNotFulledRoom(room))
-            throw OverstaffedRoomException.EXCEPTION;
     }
 
     public boolean isNotFulledRoom(Room room) {
