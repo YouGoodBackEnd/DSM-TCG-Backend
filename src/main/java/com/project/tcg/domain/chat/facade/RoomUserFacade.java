6e1889bbe5b8518cec92e86fe15c275d96b3b204
rlaisqls <rlaisqls@gmail.com>
2022-08-27 09:45:14 +0900

    ♻️ :: 메서드명 단순화

--- src/main/java/com/project/tcg/domain/chat/facade/RoomUserFacade.java
+++ src/main/java/com/project/tcg/domain/chat/facade/RoomUserFacade.java
@@ -3,13 +3,9 @@ package com.project.tcg.domain.chat.facade;
 import com.project.tcg.domain.chat.domain.Room;
 import com.project.tcg.domain.chat.domain.RoomUser;
 import com.project.tcg.domain.chat.domain.repository.RoomUserRepository;
-import com.project.tcg.domain.chat.exception.RoomNotFoundException;
-import com.project.tcg.domain.chat.exception.RoomUserAlreadyExistException;
 import com.project.tcg.domain.trade.domain.Offer;
-import com.project.tcg.domain.trade.exception.OfferImpossibleException;
 import com.project.tcg.domain.trade.presentation.dto.response.AcceptResponse;
 import com.project.tcg.domain.trade.presentation.dto.response.OfferResponse;
-import com.project.tcg.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -21,53 +17,20 @@ public class RoomUserFacade {
 
     private final RoomUserRepository roomUserRepository;
 
-    public RoomUser getRoomUserByRoomAndUser(Room room, User user) {
-        return roomUserRepository.findByRoomAndUser(room, user)
-                .orElseThrow(() -> RoomNotFoundException.EXCEPTION);
+    public void cancelAllOffer(Room room) {
+        roomUserRepository.cancelAllOffer(room);
     }
 
-    public void removeParticipatingRooms(User user) {
-        roomUserRepository.deleteAllByUser(user);
+    public void cancelAllAccept(Room room) {
+        roomUserRepository.cancelAllAccept(room);
     }
 
-    public void checkRoomUserIsNotExist(Room room, User user) {
-        if (roomUserIsExist(room, user)){
-            throw RoomUserAlreadyExistException.EXCEPTION;
-        }
-    }
-
-    public void checkRoomUserIsExist(Room room, User user) {
-        if (!roomUserIsExist(room, user)){
-            throw RoomNotFoundException.EXCEPTION;
-        }
-    }
-
-    private Boolean roomUserIsExist(Room room, User user) {
-        return roomUserRepository.countByRoomAndUser(room, user) > 0;
-    }
-
-    public void checkAllRoomUsersIsNotAccepted(Room room) {
-        if(roomUserRepository.countByRoomAndIsAcceptedTrue(room) > 0){
-            throw OfferImpossibleException.EXCEPTION;
-        }
-    }
-
-    public void makeAllRoomUserNotOfferedState(Room room) {
+    public void notifyAllOffer(Room room, Consumer<Object> sendEventConsumer) {
         room.getRoomUsers()
-                .forEach(roomUser -> roomUser.setOffer(new Offer()));
+                .forEach(roomUser -> notifyOffer(roomUser, sendEventConsumer));
     }
 
-    public void makeAllRoomUserNotAcceptedState(Room room) {
-        room.getRoomUsers()
-                .forEach(RoomUser::cancelAccept);
-    }
-
-    public void notifyAllRoomUsersOfferState(Room room, Consumer<Object> sendEventConsumer) {
-        room.getRoomUsers()
-                .forEach(roomUser -> notifyRoomUserOfferState(roomUser, sendEventConsumer));
-    }
-
-    public void notifyRoomUserOfferState(RoomUser roomUser, Consumer<Object> sendEventConsumer) {
+    public void notifyOffer(RoomUser roomUser, Consumer<Object> sendEventConsumer) {
 
         Offer offer = roomUser.getOffer() != null ? roomUser.getOffer() : new Offer();
 
@@ -83,13 +46,13 @@ public class RoomUserFacade {
         sendEventConsumer.accept(response);
     }
 
-    public void notifyAllRoomUsersAcceptState(Room room, Consumer<Object> sendEventConsumer) {
+    public void notifyAllAccept(Room room, Consumer<Object> sendEventConsumer) {
         room.getRoomUsers()
                 .forEach(roomUser ->
-                        notifyRoomUserAcceptState(roomUser, sendEventConsumer));
+                        notifyAccept(roomUser, sendEventConsumer));
     }
 
-    public void notifyRoomUserAcceptState(RoomUser roomUser, Consumer<Object> sendEventConsumer){
+    public void notifyAccept(RoomUser roomUser, Consumer<Object> sendEventConsumer){
 
         AcceptResponse response = AcceptResponse
                 .builder()
@@ -99,4 +62,5 @@ public class RoomUserFacade {
 
         sendEventConsumer.accept(response);
     }
+
 }
