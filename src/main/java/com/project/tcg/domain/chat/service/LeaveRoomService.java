d1eb7421c284b959aa03edf036757a8ab11c04cd
rlaisqls <rlaisqls@gmail.com>
2022-08-27 09:48:18 +0900

    ⚡ :: Room 페치조인으로 변경

--- src/main/java/com/project/tcg/domain/chat/service/LeaveRoomService.java
+++ src/main/java/com/project/tcg/domain/chat/service/LeaveRoomService.java
@@ -3,12 +3,13 @@ package com.project.tcg.domain.chat.service;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.project.tcg.domain.chat.domain.Room;
-import com.project.tcg.domain.chat.facade.RoomFacade;
+import com.project.tcg.domain.chat.domain.RoomUser;
 import com.project.tcg.domain.chat.facade.RoomUserFacade;
 import com.project.tcg.domain.chat.presentation.dto.response.RoomNotificationResponse;
 import com.project.tcg.domain.user.domain.User;
 import com.project.tcg.domain.user.facade.UserFacade;
 import com.project.tcg.global.socket.SocketProperty;
+import com.project.tcg.global.socket.util.SocketUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class LeaveRoomService {
 
-    private final RoomFacade roomFacade;
     private final UserFacade userFacade;
     private final RoomUserFacade roomUserFacade;
     private final SocketIOServer socketIOServer;
@@ -25,31 +25,26 @@ public class LeaveRoomService {
     @Transactional
     public void execute(SocketIOClient socketIOClient) {
 
-        User user = userFacade.getUserByClient(socketIOClient);
+        User user = userFacade.getUserAndFetchRoom(SocketUtil.getAccountId(socketIOClient));
+        RoomUser roomUser = user.getRoomUser();
+        Room room = roomUser.getRoom();
 
-        socketIOClient
-                .getAllRooms()
-                .forEach(socketRoomId -> {
-                    try {
-                        Room room = roomFacade.getRoomById(Long.valueOf(socketRoomId));
-                        room.removeUser(user);
+        room.removeRoomUser(roomUser);
 
-                        roomUserFacade.makeAllRoomUserNotAcceptedState(room);
+        roomUserFacade.cancelAllAccept(room);
 
-                        roomUserFacade.notifyAllRoomUsersAcceptState(room, (Object acceptResponse) -> {
-                            socketIOServer.getRoomOperations(room.getId().toString())
-                                    .sendEvent(SocketProperty.ACCEPT, acceptResponse);
-                        });
+        roomUserFacade.notifyAllAccept(room, (Object acceptResponse) -> {
+            socketIOServer.getRoomOperations(room.getId().toString())
+                    .sendEvent(SocketProperty.ACCEPT, acceptResponse);
+        });
 
-                        RoomNotificationResponse response =
-                                new RoomNotificationResponse(socketRoomId, user.getName() + "님이 나갔습니다.");
+        String socketRoomId = room.getId().toString();
+        socketIOClient.leaveRoom(socketRoomId);
 
-                        socketIOServer.getRoomOperations(socketRoomId)
-                                .sendEvent(SocketProperty.ROOM, response);
-                    } catch (Exception ignored){
-                    } finally {
-                        socketIOClient.leaveRoom(socketRoomId);
-                    }
-                });
+        RoomNotificationResponse response =
+                new RoomNotificationResponse(socketRoomId, user.getName() + "님이 나갔습니다.");
+        socketIOServer.getRoomOperations(socketRoomId)
+                .sendEvent(SocketProperty.ROOM, response);
     }
+
 }
