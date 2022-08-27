1147ffbc17e3818ba8a1397b9127beb432901d68
rlaisqls <rlaisqls@gmail.com>
2022-08-27 19:48:32 +0900

    🗑️ ::  테스트 쿼리 삭제
    ㅇ

--- src/main/resources/createEmoji.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO emoji
-(emoji_id, emoji_image_url, emoji_type)
-VALUES
-    (1, 'https://myawsbucketswg1214.s3.amazonaws.com/myawsbucketswg1214/32f04e30-6669-4d2d-94a8-8ccc28c83805good.png', 'EMOJI_GOOD');
-
-INSERT INTO emoji
-(emoji_id, emoji_image_url, emoji_type)
-VALUES
-    (2, 'https://myawsbucketswg1214.s3.amazonaws.com/myawsbucketswg1214/42574232-10ee-4ced-9fd1-4a06529c0a42bad.png', 'EMOJI_BAD');
