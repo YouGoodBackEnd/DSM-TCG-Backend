e8c06705da5e0a6fe3935e6adbe4a68ee72c36c2
rlaisqls <rlaisqls@gmail.com>
2022-08-27 09:47:03 +0900

    ⚡ :: UserRoomVo로 조회

--- src/main/java/com/project/tcg/domain/chat/service/SendChatService.java
+++ src/main/java/com/project/tcg/domain/chat/service/SendChatService.java
@@ -2,16 +2,15 @@ package com.project.tcg.domain.chat.service;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
-import com.project.tcg.domain.chat.domain.Room;
 import com.project.tcg.domain.chat.domain.repository.EmojiRepository;
+import com.project.tcg.domain.chat.domain.repository.vo.UserRoomVO;
 import com.project.tcg.domain.chat.exception.EmojiNotFoundException;
-import com.project.tcg.domain.chat.facade.RoomFacade;
-import com.project.tcg.domain.chat.facade.RoomUserFacade;
+import com.project.tcg.domain.chat.exception.RoomNotFoundException;
 import com.project.tcg.domain.chat.presentation.dto.request.ChatRequest;
 import com.project.tcg.domain.chat.presentation.dto.response.ChatResponse;
-import com.project.tcg.domain.user.domain.User;
-import com.project.tcg.domain.user.facade.UserFacade;
+import com.project.tcg.domain.user.domain.repository.UserRepository;
 import com.project.tcg.global.socket.SocketProperty;
+import com.project.tcg.global.socket.util.SocketUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,35 +21,30 @@ import java.time.format.FormatStyle;
 
 @RequiredArgsConstructor
 @Service
-public class ChattingService {
+public class SendChatService {
 
-    private final RoomFacade roomFacade;
-    private final RoomUserFacade roomUserFacade;
-    private final UserFacade userFacade;
+    private final UserRepository userRepository;
     private final EmojiRepository emojiRepository;
     private final SocketIOServer socketIOServer;
 
     @Transactional
     public void execute(SocketIOClient socketIOClient, ChatRequest request){
 
-        Room room = roomFacade.getRoomById(request.getRoomId());
-        User user = userFacade.getUserByClient(socketIOClient);
-
-        roomUserFacade.checkRoomUserIsExist(room, user);
+        UserRoomVO vo = userRepository.findUserAndRoomId(SocketUtil.getAccountId(socketIOClient))
+                .orElseThrow(()-> RoomNotFoundException.EXCEPTION);
 
         String emojiImageUrl = getEmojiImageUrl(request.getEmojiId());
 
         ChatResponse response = ChatResponse.builder()
-                .userId(user.getId())
-                .profileImageUrl(user.getProfileImageUrl())
-                .username(user.getName())
+                .userId(vo.getUserId())
+                .profileImageUrl(vo.getProfileImageUrl())
+                .username(vo.getUserName())
                 .emojiImageUrl(emojiImageUrl)
                 .chat(request.getChat())
                 .sentAt(LocalDateTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)))
                 .build();
 
-        String socketRoomId = room.getId().toString();
-
+        String socketRoomId = vo.getRoomId().toString();
         socketIOServer.getRoomOperations(socketRoomId)
                         .sendEvent(SocketProperty.CHAT, response);
     }
