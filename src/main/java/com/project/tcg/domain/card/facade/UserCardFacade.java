e905e991c27f6dc35755008c727017901d55cdad
rlaisqls <rlaisqls@gmail.com>
2022-09-18 23:30:59 +0900

    ♻️ :: 필요없는 메서드 삭제

--- src/main/java/com/project/tcg/domain/card/facade/UserCardFacade.java
+++ src/main/java/com/project/tcg/domain/card/facade/UserCardFacade.java
@@ -11,10 +11,6 @@ public class UserCardFacade {
 
     private final UserCardRepository userCardRepository;
 
-    public Boolean checkUserCardExist(User user, Long cardId){
-        return userCardRepository.findFirstByUserAndCard_id(user,cardId).isPresent();
-    }
-
     public int getUserCardCount(Long cardId, User user) {
         return userCardRepository.findByCard_IdAndUser(cardId, user).size();
     }
