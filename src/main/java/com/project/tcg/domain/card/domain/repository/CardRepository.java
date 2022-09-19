5666bbe4d15ece1066b76d2acd3f55f91b1983f5
rlaisqls <rlaisqls@gmail.com>
2022-09-19 23:34:07 +0900

    ♻️ :: 가독성 개선

--- src/main/java/com/project/tcg/domain/card/domain/repository/CardRepository.java
+++ src/main/java/com/project/tcg/domain/card/domain/repository/CardRepository.java
@@ -10,6 +10,5 @@ import java.util.Optional;
 public interface CardRepository extends CrudRepository<Card, Long> {
 
     List<Card> findByGrade(Grade grade);
-
     Optional<Card> findById(Long id);
 }
