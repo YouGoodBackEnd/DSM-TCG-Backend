ceee29d8cf20365f3f93dece6622591965ae692b
rlaisqls <rlaisqls@gmail.com>
2022-10-10 14:01:29 +0900

    ⚡ :: total count 추가

--- src/main/java/com/project/tcg/domain/card/presentation/dto/response/QueryUserCardListResponse.java
+++ src/main/java/com/project/tcg/domain/card/presentation/dto/response/QueryUserCardListResponse.java
@@ -1,15 +1,20 @@
 package com.project.tcg.domain.card.presentation.dto.response;
 
 import com.project.tcg.domain.user.domain.CardCount;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 public class QueryUserCardListResponse {
 
     private CardCount cardCount;
+    private Integer totalCount;
     private List<UserCardResponse> cardList;
+
+    public QueryUserCardListResponse(CardCount cardCount, List<UserCardResponse> cardList) {
+        this.cardCount = cardCount;
+        this.cardList = cardList;
+        this.totalCount = cardList.size();
+    }
 }
