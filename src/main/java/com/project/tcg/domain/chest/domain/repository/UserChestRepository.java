990245d7cc6084f1e9428233d3ff111c3687c23b
rlaisqls <rlaisqls@gmail.com>
2022-12-13 08:22:01 +0900

    ⚡ :: deleteByUser

--- src/main/java/com/project/tcg/domain/chest/domain/repository/UserChestRepository.java
+++ src/main/java/com/project/tcg/domain/chest/domain/repository/UserChestRepository.java
@@ -4,4 +4,6 @@ import com.project.tcg.domain.chest.domain.UserChest;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserChestRepository extends CrudRepository<UserChest, Long> {
+
+    void deleteByUser(User user);
 }
--- src/main/java/com/project/tcg/domain/user/service/WithdrawalService.java
+++ src/main/java/com/project/tcg/domain/user/service/WithdrawalService.java
@@ -3,6 +3,8 @@ package com.project.tcg.domain.user.service;
 
 import com.project.tcg.domain.user.domain.User;
 import com.project.tcg.domain.user.domain.repository.UserRepository;
+import com.project.tcg.domain.card.domain.repository.UserCardRepository;
+import com.project.tcg.domain.chest.domain.repository.UserChestRepository;
 import com.project.tcg.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,12 +16,19 @@ public class WithdrawalService {
 
     private final UserRepository userRepository;
 
+    private final UserCardRepository userCardRepository;
+
+    private final UserChestRepository userChestRepository;
+
     private final UserFacade userFacade;
 
     @Transactional
     public void execute() {
 
         User user = userFacade.getCurrentUser();
+
+        userCardRepository.deleteByUser(user);
+        userChestRepository.deleteByUser(user);
 
         userRepository.delete(user);
     }
