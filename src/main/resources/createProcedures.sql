91ca1bb2015069baeb1e90a776e199462c18a774
rlaisqls <rlaisqls@gmail.com>
2022-12-15 16:49:27 +0900

    ⚡ :: 랭킹 날짜별 저장

--- src/main/resources/createProcedures.sql
+++ src/main/resources/createProcedures.sql
@@ -1,23 +1,23 @@
 CREATE PROCEDURE SAVE_RANK()
 BEGIN
-    INSERT INTO ranks(
+    INSERT INTO tcg.ranks(
         created_at, user_id, account_id, name, profile_image_url, ranking,
-        ss_grade_card_count, s_grade_card_count, a_grade_card_count, b_grade_card_count, c_grade_card_count
+        s_grade_card_count, a_grade_card_count, b_grade_card_count, c_grade_card_count, d_grade_card_count
     ) SELECT
-          DATE_SUB(DATE_SUB(now(), interval(SECOND(now())) SECOND), interval(MINUTE(now())) MINUTE) AS created_at,
-          user.user_id,
-          user.account_id ,
-          user.name,
-          user.profile_image_url AS profile_image_url,
-          ROW_NUMBER() OVER (ORDER BY user.s_grade_card_count DESC,
-                                        user.a_grade_card_count DESC,
-                                        user.b_grade_card_count DESC,
-                                        user.c_grade_card_count DESC,
-                                        user.d_grade_card_count DESC) AS ranking,
-          user.s_grade_card_count,
-          user.a_grade_card_count,
-          user.b_grade_card_count,
-          user.c_grade_card_count,
-          user.d_grade_card_count
-    FROM user;
+        DATE(now()) AS created_at,
+        user.user_id,
+        user.account_id ,
+        user.name,
+        user.profile_image_url AS profile_image_url,
+        ROW_NUMBER() OVER (ORDER BY user.s_grade_card_count DESC,
+            user.a_grade_card_count DESC,
+            user.b_grade_card_count DESC,
+            user.c_grade_card_count DESC,
+            user.d_grade_card_count DESC) AS ranking,
+        user.s_grade_card_count,
+        user.a_grade_card_count,
+        user.b_grade_card_count,
+        user.c_grade_card_count,
+        user.d_grade_card_count
+    FROM tcg.user;
 END$$
